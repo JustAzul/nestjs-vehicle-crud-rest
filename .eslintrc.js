c05cf694979269e3106bafe3e714a03ae79fbcde
Diego (Azul) Ferreira <metalloger@gmail.com>
2024-11-29 10:22:06 -0300

    🔧 (eslint): enhance linting configuration with additional plugins and rules
    Add plugins for 'prettier', 'import', 'sort-keys', and 'typescript-sort-keys' to improve code quality and consistency. Extend ESLint configuration with 'eslint:recommended' and 'plugin:import/recommended' for better import management. Update rules to enforce prettier formatting, key sorting, and import order. Upgrade related dependencies in package.json to ensure compatibility and leverage new features.
    
    These changes aim to enforce a more consistent code style, improve readability, and prevent potential errors by adhering to recommended practices and maintaining an organized codebase.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,19 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'prettier',
+    'import',
+    'sort-keys',
+    'typescript-sort-keys',
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   root: true,
   env: {
@@ -21,5 +30,24 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error',
+    'sort-keys/sort-keys-fix': 'error',
+    'typescript-sort-keys/interface': 'error',
+    'typescript-sort-keys/string-enum': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
   },
 };
