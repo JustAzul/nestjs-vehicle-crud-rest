92f129ace091ae6f14e03aefed0d4b548b700f0c
Diego (Azul) Ferreira <metalloger@gmail.com>
2024-11-30 18:53:44 -0300

    ✨ (vehicle.module.ts): add in-memory vehicle repository for dependency injection
    Introduce an in-memory vehicle repository to the VehicleModule, allowing for dependency injection of the IVehicleRepository interface. This change enhances the module's flexibility and testability by enabling the use of different repository implementations without altering the module's core logic.

--- src/vehicle/vehicle.module.ts
+++ src/vehicle/vehicle.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 
 import { VehicleController } from './vehicle.controller';
+import { IVehicleRepository } from './repositories/interfaces/vehicle.repository';
+import { InMemoryVehicleRepository } from './repositories/in-memory.vehicle.repository';
 
 @Module({
   controllers: [VehicleController],
+  providers: [
+    {
+      provide: IVehicleRepository,
+      useFactory: () => {
+        return new InMemoryVehicleRepository();
+      },
+    },
+  ],
 })
 export class VehicleModule {}
