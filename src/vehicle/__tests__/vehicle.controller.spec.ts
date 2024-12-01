53202eccf85f10630b0e9604a29cbb1d831cb6e0
Diego (Azul) Ferreira <metalloger@gmail.com>
2024-12-01 20:41:14 -0300

    ✅ (vehicle.controller.spec.ts): enhance test structure and data setup for VehicleController E2E tests
    Refactor the VehicleController E2E tests to improve clarity and maintainability. The test suite now uses a more descriptive name for the controller, aligning with best practices. The vehicle data setup is moved to a higher scope to avoid redundancy, and the test initialization is streamlined by using `beforeEach` and `afterEach` hooks. This ensures a clean state for each test run, enhancing test reliability. Additionally, the removal of `randomUUID` import, which was unused, cleans up the code.

--- src/vehicle/__tests__/vehicle.controller.spec.ts
+++ src/vehicle/__tests__/vehicle.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { randomUUID, UUID } from 'crypto';
+import { UUID } from 'crypto';
 import { expect } from 'chai';
 import { InMemoryVehicleRepository } from '../repositories/in-memory.vehicle.repository';
 import { IVehicleRepository } from '../repositories/interfaces/vehicle.repository';
@@ -9,74 +9,71 @@ import { VehicleModule } from '../vehicle.module';
 import { VEHICLE_UNIQUE_FIELDS } from '../constants/module.contants';
 import { Vehicle, VehicleProps } from '../entities/vehicle.entity';
 import { ListVehicleData } from '../dto/list-vehicle.dto';
+import { VehicleController } from '../vehicle.controller';
 
-describe('VehicleController (e2e)', () => {
+describe(`${VehicleController.name} (E2E)`, () => {
   let repositorySourceData: Map<UUID, Vehicle>;
   let app: INestApplication;
   let repository: IVehicleRepository;
 
-  before(async () => {
-    repositorySourceData = new Map();
-
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [VehicleModule],
-      providers: [
-        {
-          provide: IVehicleRepository,
-          useFactory: () => {
-            return new InMemoryVehicleRepository(
-              repositorySourceData,
-              VEHICLE_UNIQUE_FIELDS,
-            );
-          },
-        },
-      ],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    repository = moduleFixture.get<IVehicleRepository>(IVehicleRepository);
-
-    await app.init();
-  });
-
-  after(async () => {
-    await app.close();
-  });
+  const vehicles: VehicleProps[] = [
+    {
+      brand: 'Toyota',
+      chassis: '123',
+      model: 'Corolla',
+      plate: 'XYZ123',
+      renavam: '5678',
+      year: 2020,
+    },
+    {
+      brand: 'Honda',
+      chassis: '456',
+      model: 'Civic',
+      plate: 'XYZ456',
+      renavam: '6789',
+      year: 2021,
+    },
+    {
+      brand: 'Ford',
+      chassis: '789',
+      model: 'Focus',
+      plate: 'XYZ789',
+      renavam: '7890',
+      year: 2019,
+    },
+  ];
 
   describe('GET /vehicle', () => {
-    const vehicles: VehicleProps[] = [
-      {
-        brand: 'Toyota',
-        chassis: '123',
-        model: 'Corolla',
-        plate: 'XYZ123',
-        renavam: '5678',
-        year: 2020,
-      },
-      {
-        brand: 'Honda',
-        chassis: '456',
-        model: 'Civic',
-        plate: 'XYZ456',
-        renavam: '6789',
-        year: 2021,
-      },
-      {
-        brand: 'Ford',
-        chassis: '789',
-        model: 'Focus',
-        plate: 'XYZ789',
-        renavam: '7890',
-        year: 2019,
-      },
-    ];
-
     beforeEach(async () => {
+      repositorySourceData = new Map();
+
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [VehicleModule],
+        providers: [
+          {
+            provide: IVehicleRepository,
+            useFactory: () => {
+              return new InMemoryVehicleRepository(
+                repositorySourceData,
+                VEHICLE_UNIQUE_FIELDS,
+              );
+            },
+          },
+        ],
+      }).compile();
+
+      app = moduleFixture.createNestApplication();
+      repository = moduleFixture.get<IVehicleRepository>(IVehicleRepository);
+
+      await app.init();
+
       for (const vehicle of vehicles) {
-        await repository.create({
-          entity: vehicle,
-        });
+        await repository.create({ entity: vehicle });
       }
+    });
+
+    afterEach(async () => {
+      await app.close();
     });
 
     it('should return the first page of vehicles', async () => {
@@ -92,6 +89,12 @@ describe('VehicleController (e2e)', () => {
 
       expect(body.data).to.be.an('array');
       expect(body.data.length).to.equal(2);
+
+      // @ts-expect-error - data is private
+      expect(body.data.map(({ props }) => props)).to.deep.include.members([
+        vehicles[0],
+        vehicles[1],
+      ]);
 
       expect(body.metadata).to.be.an('object');
       expect(body.metadata.page).to.equal(String(page));
