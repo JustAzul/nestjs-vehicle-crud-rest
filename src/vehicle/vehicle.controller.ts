d14a0ed9c08231a5da3fe3f99dae9881db9e9a2a
Diego (Azul) Ferreira <metalloger@gmail.com>
2024-12-02 12:37:48 -0300

    ♻️ (vehicle.controller.ts): refactor exception handling and use HttpStatus for API responses
    Refactor exception handling to streamline error messages by using a single `InternalServerErrorException` constructor. Replace hardcoded status codes with `HttpStatus` constants for better readability and maintainability. This change enhances code clarity and aligns with best practices for error handling and response status management.

--- src/vehicle/vehicle.controller.ts
+++ src/vehicle/vehicle.controller.ts
@@ -6,12 +6,13 @@ import {
   Delete,
   Param,
   Body,
-  NotImplementedException,
   ValidationPipe,
   Query,
+  NotFoundException,
   InternalServerErrorException,
   BadRequestException,
-  NotFoundException,
+  HttpStatus,
+  NotImplementedException,
 } from '@nestjs/common';
 import { UUID } from 'crypto';
 import { CreateVehicleDataDto } from './dto/create-vehicle-data.dto';
@@ -29,18 +30,20 @@ import { VehicleMapper } from './vehicle.mapper';
 export class VehicleController {
   constructor(private readonly vehicleRepository: IVehicleRepository) {}
 
-  // GET /vehicle - List all vehicles with pagination
   @Get()
   @ApiOperation({
     summary: 'Get all vehicles',
     description: 'Retrieve a paginated list of all vehicles.',
   })
   @ApiResponse({
-    status: 200,
+    status: HttpStatus.OK,
     description: 'List of vehicles retrieved successfully.',
     type: ListVehicleData,
   })
-  @ApiResponse({ status: 400, description: 'Invalid page or page size.' })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Invalid page or page size.',
+  })
   async getAllVehicles(
     @Query('page') page: number = 1,
     @Query('pageSize') pageSize: number = DEFAULT_PAGE_SIZE,
@@ -66,25 +69,25 @@ export class VehicleController {
         }
       }
 
-      if (e instanceof Error) {
-        throw new InternalServerErrorException(e.message, e.stack);
-      }
-
-      throw new InternalServerErrorException(e);
+      throw new InternalServerErrorException(
+        e instanceof Error ? e.message : e,
+      );
     }
   }
 
-  // GET /vehicle/:id - Get a specific vehicle by ID
   @Get(':id')
   @ApiOperation({
     summary: 'Get vehicle by ID',
     description: 'Retrieve details of a specific vehicle by its ID.',
   })
   @ApiResponse({
-    status: 200,
+    status: HttpStatus.OK,
     description: 'Vehicle details retrieved successfully.',
   })
-  @ApiResponse({ status: 404, description: 'Vehicle not found.' })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Vehicle not found.',
+  })
   @ApiParam({
     name: 'id',
     description: 'The unique identifier of the vehicle (UUID).',
@@ -99,27 +102,40 @@ export class VehicleController {
     return VehicleMapper.toDTO(vehicle);
   }
 
-  // POST /vehicle - Create a new vehicle
   @Post()
   @ApiOperation({
     summary: 'Create a new vehicle',
     description: 'Create a new vehicle with the provided data.',
   })
-  @ApiResponse({ status: 201, description: 'Vehicle created successfully.' })
-  @ApiResponse({ status: 400, description: 'Invalid input data.' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Vehicle created successfully.',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Invalid input data.',
+  })
   createVehicle(@Body(ValidationPipe) vehicleData: CreateVehicleDataDto) {
     throw new NotImplementedException('Method not implemented');
   }
 
-  // PUT /vehicle/:id - Update a specific vehicle by ID
   @Put(':id')
   @ApiOperation({
     summary: 'Update a vehicle',
     description: 'Update details of an existing vehicle by its ID.',
   })
-  @ApiResponse({ status: 200, description: 'Vehicle updated successfully.' })
-  @ApiResponse({ status: 404, description: 'Vehicle not found.' })
-  @ApiResponse({ status: 400, description: 'Invalid input data.' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Vehicle updated successfully.',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Vehicle not found.',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Invalid input data.',
+  })
   @ApiParam({
     name: 'id',
     description: 'The unique identifier of the vehicle (UUID).',
@@ -131,14 +147,19 @@ export class VehicleController {
     throw new NotImplementedException('Method not implemented');
   }
 
-  // DELETE /vehicle/:id - Delete a specific vehicle by ID
   @Delete(':id')
   @ApiOperation({
     summary: 'Delete a vehicle',
     description: 'Delete a specific vehicle by its ID.',
   })
-  @ApiResponse({ status: 200, description: 'Vehicle deleted successfully.' })
-  @ApiResponse({ status: 404, description: 'Vehicle not found.' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Vehicle deleted successfully.',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Vehicle not found.',
+  })
   @ApiParam({
     name: 'id',
     description: 'The unique identifier of the vehicle (UUID).',
@@ -159,10 +180,10 @@ export class VehicleController {
             throw new InternalServerErrorException(e.message, e.stack);
         }
       }
-      if (e instanceof Error) {
-        throw new InternalServerErrorException(e.message, e.stack);
-      }
-      throw new InternalServerErrorException(e);
+
+      throw new InternalServerErrorException(
+        e instanceof Error ? e.message : e,
+      );
     }
   }
 }
