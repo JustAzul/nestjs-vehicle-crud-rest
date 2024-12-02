289daab431688729e10a6ae1ac07a2c21e80172e
Diego (Azul) Ferreira <metalloger@gmail.com>
2024-12-02 14:31:49 -0300

    ♻️ (vehicle.module.ts): refactor provider definition for VehicleRepository
    Extract the VehicleRepository provider into a separate constant to improve readability and maintainability. This change simplifies the module's structure by clearly separating the provider's configuration from the module's metadata, making it easier to manage and update in the future.

--- src/vehicle/vehicle.module.ts
+++ src/vehicle/vehicle.module.ts
@@ -1,19 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, Provider } from '@nestjs/common';
 
 import { VehicleController } from './vehicle.controller';
 import { IVehicleRepository } from './repositories/interfaces/vehicle.repository';
 import { InMemoryVehicleRepository } from './repositories/in-memory.vehicle.repository';
 import { VEHICLE_UNIQUE_FIELDS } from './constants/module.contants';
 
+const VehicleRepository: Provider = {
+  provide: IVehicleRepository,
+  useFactory: () => {
+    return new InMemoryVehicleRepository(new Map(), VEHICLE_UNIQUE_FIELDS);
+  },
+};
+
 @Module({
   controllers: [VehicleController],
-  providers: [
-    {
-      provide: IVehicleRepository,
-      useFactory: () => {
-        return new InMemoryVehicleRepository(new Map(), VEHICLE_UNIQUE_FIELDS);
-      },
-    },
-  ],
+  providers: [VehicleRepository],
 })
 export class VehicleModule {}
